98fce6575b73e6778370eca81deefce63c9ea1b1
Max Maischein <corion@corion.net>
2010-10-18 19:13:51 +0200

    Bump version

--- lib/MozRepl/RemoteObject.pm
+++ lib/MozRepl/RemoteObject.pm
@@ -39,7 +39,7 @@ MozRepl::RemoteObject - treat Javascript objects as Perl objects
 =cut
 
 use vars qw[$VERSION $objBridge @CARP_NOT];
-$VERSION = '0.13';
+$VERSION = '0.14';
 
 @CARP_NOT = (qw[MozRepl::RemoteObject::Instance
                 MozRepl::RemoteObject::TiedHash
