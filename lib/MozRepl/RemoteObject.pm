ace995a31bb503d811f9fb59a3381656e971d7ff
Max Maischein <corion@corion.net>
2011-01-06 20:27:15 +0100

    Bump copyright

--- lib/MozRepl/RemoteObject.pm
+++ lib/MozRepl/RemoteObject.pm
@@ -1769,7 +1769,7 @@ Max Maischein C<corion@cpan.org>
 
 =head1 COPYRIGHT (c)
 
-Copyright 2009-2010 by Max Maischein C<corion@cpan.org>.
+Copyright 2009-2011 by Max Maischein C<corion@cpan.org>.
 
 =head1 LICENSE
 
