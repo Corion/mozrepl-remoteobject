5a8d8c8f7f940d6da28674f4e6a9d8e5a2cc03eb
Max Maischein <corion@corion.net>
2011-08-21 13:01:39 +0200

    Reformat whitespace

--- lib/MozRepl/RemoteObject.pm
+++ lib/MozRepl/RemoteObject.pm
@@ -413,8 +413,10 @@ sub install_bridge {
     $options{ queue } ||= [];
     $options{ bufsize } ||= 10_240_000;
     $options{ use_queue } ||= 0; # > 0 means enqueue
-    $options{ max_queue_size } ||= 1000; # mozrepl
-                                         # / Net::Telnet don't like too large commands
+    # mozrepl
+    # / Net::Telnet don't like too large commands
+    $options{ max_queue_size } ||= 1000;
+    
 
     if (! ref $options{repl}) { # we have host:port
         my @host_port;
